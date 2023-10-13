19fd921ae3ac31f53cd1b9b1e795e9ae6c134050
baru-d <barudaniel6@gmail.com>
2023-10-13 14:01:51 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.getElementById('main').remove();
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+const myname = 'esra';
+newHeader.innerHTML = `${myname} is the champion`;
